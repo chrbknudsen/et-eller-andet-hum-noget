d89b74b48614e8ddaccf599f58ecd041ac9ac7bb
Christian B. Knudsen <christian.knudsen@gmail.com>
2024-06-07 10:02:38 +0200

    Initial commit

---
+++ scripts/data_til_asger.R
@@ -0,0 +1,25 @@
+# Datatræk til Asger
+# Træk af places, udgivere og forfattere mhp konsolidering af navneformer
+
+dan_filter_meta %>% 
+  select(place) %>% 
+  distinct() %>% 
+  write_csv("data_out/distinct_places.csv")
+
+
+  
+
+dan_filter_meta %>% 
+  select(author1:author2, year, `Subject code` ) %>% 
+  pivot_longer(author1:author2, names_to = "type",
+               values_to = "forfatternavn") %>% 
+  select(-type) %>% 
+  filter(!is.na(forfatternavn)) %>% 
+  write_csv("data_out/forfatternavne.csv")
+
+
+dan_filter_meta %>% 
+  select(publisher, place) %>% 
+  filter(!is.na(place)) %>% 
+  write_csv("data_out/udgivernavne.csv")
+  

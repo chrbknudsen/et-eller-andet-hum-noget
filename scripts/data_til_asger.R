bd01f13bfc83f420e186a05cb41b1d92194d352b
Christian B. Knudsen <christian.knudsen@gmail.com>
2024-06-07 10:21:23 +0200

    Emnekoder tilføjet til forfattere

--- scripts/data_til_asger.R
+++ scripts/data_til_asger.R
@@ -10,14 +10,17 @@ dan_filter_meta %>%
   write_csv("data_out/distinct_places.csv")
 
 
-  
+temp_emnekoder <- read_csv("data/kb_emnekoder.csv") %>% 
+  distinct(`Subject code`, .keep_all = T)
 
 dan_filter_meta %>% 
   select(author1:author2, year, `Subject code` ) %>% 
   pivot_longer(author1:author2, names_to = "type",
                values_to = "forfatternavn") %>% 
   select(-type) %>% 
-  filter(!is.na(forfatternavn)) %>% 
+  filter(!is.na(forfatternavn)) %>%
+  left_join(temp_emnekoder) %>% 
+  
   write_csv("data_out/forfatternavne.csv")
 
 
